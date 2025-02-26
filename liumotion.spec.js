5108ee40b64180175b42e10cfeb2087b18e14679
Novecento99 <61213759+Novecento99@users.noreply.github.com>
2025-02-26 19:07:59 +0100

    Add initial test suite for LiuMotion

---
+++ liumotion.spec.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const LiuMotion = require('./liumotion');
+
+describe('LiuMotion', function () {
+    it('dummy test that always passes', function () {
+        assert.strictEqual(true, true);
+    });
+});
