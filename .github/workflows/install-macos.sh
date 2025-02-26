4bbe36b5eaa5d0421ef916c0a58562b636e40862
Novecento99 <61213759+Novecento99@users.noreply.github.com>
2025-02-26 18:53:49 +0100

    Add installation scripts for Linux, macOS, and Windows

---
+++ .github/workflows/install-macos.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -x
+
+which python3
+python3 -c 'import sys; print(sys.version)'
+
+pip3 install .
+
+# pyinstaller needs to have the extensions built explicitely
+python3 setup.py build_ext --inplace
+
+# for Macos Big Sur, the stable portaudio (19.6.0) makes liumotion freeze on startup
+# install from latest master instead
+# see: https://github.com/tlecomte/liumotion/issues/154
+brew install portaudio --HEAD
+
+pyinstaller liumotion.spec -y
+
+ls -la dist/*
+
+# compare the portaudio libs to make sure the package contains the one that was installed with brew
+ls -la /usr/local/lib/libportaudio.dylib
+ls -la /usr/local/Cellar/portaudio/*/lib/libportaudio*.dylib
+ls -la dist/liumotion.app/Contents/Frameworks/_sounddevice_data/portaudio-binaries
+
+# PyInstaller will try to codesign liumotion.app, but will fail because of the Qt5 file structure
+# (this is visible in the logs)
+# see https://github.com/pyinstaller/pyinstaller/wiki/Recipe-OSX-Code-Signing-Qt
+# so we fix the folder names and then sign again manually
+python3 installer/fix_app_qt_folder_names_for_codesign.py dist/liumotion.app
+codesign -s - --force --all-architectures --timestamp --deep dist/liumotion.app
+codesign -dv dist/liumotion.app
+
+# prepare a dmg out of liumotion.app
+export ARTIFACT_FILENAME=liumotion-$(python3 -c 'import liumotion; print(liumotion.__version__)')-$(date +'%Y%m%d').dmg
+echo $ARTIFACT_FILENAME
+
+# macos has random hdiutil errors because of XProtectBehaviorService, retry a few times.
+# Reference: https://github.com/actions/runner-images/issues/7522#issuecomment-1556766641
+max_retries=5
+retry_delay_seconds=5
+
+for ((i=1; i<=max_retries; i++)); do
+    set +e  # Disable 'exit on error'
+    hdiutil create $ARTIFACT_FILENAME -volname "liumotion" -fs HFS+ -srcfolder dist/liumotion.app
+    status=$?
+    set -e  # Re-enable 'exit on error'
+
+    if [ $status -eq 0 ]; then
+        echo "hdiutil create succeeded on attempt $i"
+        break
+    else
+        echo "hdiutil create failed on attempt $i"
+        if [ $i -lt $max_retries ]; then
+            echo "Retrying in $retry_delay_seconds seconds..."
+            sleep $retry_delay_seconds
+        else
+            echo "All attempts failed."
+            exit 1
+        fi
+    fi
+done
+
+du -hs dist/liumotion.app
+du -hs $ARTIFACT_FILENAME
